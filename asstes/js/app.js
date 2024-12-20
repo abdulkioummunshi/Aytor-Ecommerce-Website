496ebd3f5f0f09ed6ca0c1d2feb94d3a6adcb05e
Abdul Kioum Munshi <165002745+webtk55@users.noreply.github.com>
2024-12-20 10:25:52 +0600

    20-12-2024

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -1,40 +1,4 @@
 
-
-
-const targetDate = new Date("2025-12-15T23:59:59").getTime();
-
-// Update function
-function updateCountdown() {
-    const now = new Date().getTime();
-    const difference = targetDate - now;
-
-    if (difference <= 0) {
-        clearInterval(timer);
-        document.querySelector(".deals-time").innerHTML = "<h4>🎉 Happy New Year! 🎉</h4>";
-        return;
-    }
-
-    // Calculate time parts
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-
-    // Update HTML
-    document.querySelectorAll(".deals-time ul li h4")[0].innerText = days;
-    document.querySelectorAll(".deals-time ul li h4")[1].innerText = hours;
-    document.querySelectorAll(".deals-time ul li h4")[2].innerText = minutes;
-    document.querySelectorAll(".deals-time ul li h4")[3].innerText = seconds;
-}
-
-// Run the countdown every second
-const timer = setInterval(updateCountdown, 1000);
-
-// Call immediately to prevent delay
-updateCountdown();
-
-
-
 let dropDown = document.querySelector('#dropdown');
 let dropMenu = document.querySelector('.dropdown');
 
